6b43e5b49225f31bdfbfe89a79600759c682a388
Cali93 <c.armut93@outlook.com>
2018-01-19 14:56:15 +0100

    Dorothy 0.2

---
+++ dorothy-src/src/app/components/message-item/message-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '@app/models';
+
+@Component({
+  selector: 'message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.scss']
+})
+export class MessageItemComponent implements OnInit {
+
+  @Input('message')
+  private message: Message;
+
+  constructor() { }
+
+  ngOnInit() {
+    window.setInterval(function() {
+      const elem = document.getElementById('data');
+      elem.scrollTop = elem.scrollHeight;
+    }, 1000);
+  }
+
+}
---
+++ dorothy-src/src/typings.d.ts
@@ -0,0 +1,6 @@
+/* SystemJS module definition */
+declare var module: NodeModule;
+interface NodeModule {
+  particlesJS: any;
+  id: string;
+}
